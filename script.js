3fa1faa25192e2599adba530f66a179eca6d5bd3
Rob Hingstrum <rhingstrumjr@gmail.com>
2021-12-30 16:09:18 -0600

    Refactor to get title, author, and pages

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 let myLibrary = [];
 
 const book = (title, author, pages, haveRead) => {
-    const book = {
-        info : () => { 
-            return `${title} by ${author}, ${pages} pages, ${book.haveRead ? 'have read' : 'not read yet'}`
-            },
+    return {
+        get title() {
+            return title
+        },
+        get author() {
+            return author
+        },
+        get pages() {
+            return pages
+        },
         haveRead
-    };
-    return book
+    }
 }
 
 function addBookToLibrary () {
-    book(document.getElementById('title').value, document.getElementById('author').value, document.getElementById('pages').value, document.getElementById('read').checked)
+    return myLibrary.push(book(document.getElementById('title').value, document.getElementById('author').value, document.getElementById('pages').value, document.getElementById('read').checked))
 }
 
-const harryPotter = book('Chamber of Secrets', 'JK Rowling', 350, true);
+// const harryPotter = book('Chamber of Secrets', 'JK Rowling', 350, true);
+document.getElementById("addBook").addEventListener('click', () => addBookToLibrary())
