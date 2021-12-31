beb1a6422f5ad5c6d1ff41a713485a0f3db63b08
Rob Hingstrum <rhingstrumjr@gmail.com>
2021-12-30 21:32:27 -0600

    Add book only if title, author, and pages have been entered

--- script.js
+++ script.js
@@ -16,15 +16,20 @@ const book = (title, author, pages, haveRead) => {
 }
 
 function addBookToLibrary () {
-    let title = document.getElementById('title');
-    let author = document.getElementById('author');
-    let pages = document.getElementById('pages');
-    let haveRead = document.getElementById('read');
-    myLibrary.push(book(title.value, author.value, pages.value, haveRead.checked))
-    title.value = "";
-    author.value = "";
-    pages.value = "";
-    haveRead.checked = false;
+    const author = document.getElementById('author');
+    const title = document.getElementById('title');
+    const pages = document.getElementById('pages');
+    const haveRead = document.getElementById('read');
+    if (title.value && author.value && pages.value) {
+        myLibrary.push(book(title.value, author.value, pages.value, haveRead.checked))
+        title.value = "";
+        author.value = "";
+        pages.value = "";
+        haveRead.checked = false;
+    } else {
+        alert("Missing title, author, and/or pages.")
+    }
+    
 }
 
 // const harryPotter = book('Chamber of Secrets', 'JK Rowling', 350, true);
