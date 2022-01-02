7848178dd38b7857190634636fab0f4ea35802d3
Rob Hingstrum <rhingstrumjr@gmail.com>
2022-01-01 21:56:18 -0600

    Add and remove books; change read status

--- script.js
+++ script.js
@@ -27,26 +27,46 @@ function addBookToLibrary () {
         pages.value = "";
         haveRead.checked = false;
         displayBooks();
+        document.querySelector("menu").style.display = "none";
     } else {
         alert("Missing title, author, and/or pages.")
     }
 }
 
-function displayBooks () {
+function displayBooks (startingIndex = myLibrary.length - 1) {
     const shelf = document.getElementById("shelf");
     myLibrary.forEach( (book, i) => {
-        const card = document.createElement('div');
-        card.setAttribute("data-loc", `${i}`);
-        card.classList.add("card");
-        card.innerHTML = `
-            <h4>${book.title}</h5>
-            <h5>${book.author}</h5>
-            <p>${book.pages} pages</p>
-            <label>Read? <button onclick="changeReadStatus(${i})">${book.haveRead ? "Yes" : "No"}</button></label>
-            <button onclick="removeBook(${i})">Remove from library</button>`;
-        shelf.appendChild(card)
+        if (i >= startingIndex) {
+            const card = document.createElement('div');
+            card.setAttribute("data-loc", `${i}`);
+            card.classList.add("card");
+            card.innerHTML = `
+                <h4>${book.title}</h5>
+                <h5>${book.author}</h5>
+                <p>${book.pages} pages</p>
+                <label>Read? <button data-loc="${i}" onclick="changeReadStatus(${i})">${book.haveRead ? "Yes" : "No"}</button></label>
+                <button onclick="removeBook(${i})">Remove from library</button>`;
+            shelf.appendChild(card)
+        } 
     })
 }
 
+function removeBook(index) {
+    for (let k = index; k < myLibrary.length; k++) {
+        let el = document.querySelector(`div[data-loc='${k}']`)
+        el.remove()
+    }
+    myLibrary.splice(index, 1)
+    displayBooks(index)
+}
+
+function changeReadStatus(index) {
+    myLibrary[index].haveRead = !myLibrary[index].haveRead;
+    const el = document.querySelector(`button[data-loc='${index}']`)
+    el.innerText == "Yes" ? el.innerText = "No" : el.innerText = "Yes";
+}
 // const harryPotter = book('Chamber of Secrets', 'JK Rowling', 350, true);
 document.getElementById("addBook").addEventListener('click', () => addBookToLibrary())
+document.getElementById("showMenu").addEventListener('click', () => {
+    document.querySelector("menu").style.display = "block";
+})
